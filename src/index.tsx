79132a814aba09747fe18f111cd9478930593544
masashi.g <masashi.g@gmail.com>
2020-06-20 14:18:26 +0900

    add types for tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,91 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Video from './video'; // video.tsx
+
+import { MouseEvent } from 'react';
+
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+
+// ------ App class ------
+
+class App extends React.Component {
+  localStream: MediaStream | null;
+
+  constructor(props: object) {
+    super(props);
+    this.localStream = null;
+    this.state = {
+      playing: false,
+    };
+
+    // This binding is necessary to make `this` work in the callback
+    this.startVideo = this.startVideo.bind(this);
+    this.stopVideoHandler = this.stopVideoHandler.bind(this);
+  }
+
+  componentDidMount() {
+    console.log('App DidMound()');
+  }
+
+  componentWillUnmount() {
+    console.log('App WillUnmount()');
+    if (this.localStream) {
+      this.stopVideo();
+    }
+  }
+
+  // -----------
+
+  startVideo(e: MouseEvent<HTMLButtonElement>) {
+    e.preventDefault();
+    console.log('start Video');
+    if (this.localStream) {
+      console.warn('localVideo ALREADY started');
+      return;
+    }
+
+    const constraints = { video: true, audio: false };
+    navigator.mediaDevices.getUserMedia(constraints)
+      .then(stream => {
+        this.localStream = stream;
+        this.setState({ playing: true });
+      })
+      .catch(err => console.error('media ERROR:', err));
+  }
+
+  stopVideoHandler(e: MouseEvent<HTMLButtonElement>) {
+    e.preventDefault();
+    console.log('stop Video');
+    this.stopVideo();
+  }
+
+  stopVideo() {
+    if (this.localStream) {
+      this.localStream.getTracks().forEach(track => track.stop());
+      this.localStream = null;
+      this.setState({ playing: false });
+    }
+  }
+
+  render() {
+    console.log('App render()');
+    return (
+      <div className="App" >
+        React getUserMedia example<br />
+        <button onClick={this.startVideo}> Start Video</button >
+        <button onClick={this.stopVideoHandler}>Stop Video</button>
+        <div className="VideoContainer">
+          <Video id={"local_video"} width={"240px"} height={"180px"} stream={this.localStream} volume={0} controls={false}>
+          </Video>
+        </div>
+      </div >
+    );
+  }
+}
+
+// ====================== ReactDOM rendering ====================
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <App />,
   document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
